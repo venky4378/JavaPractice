88908d38bda159bb328249ce2a1e8682fdd26bd5
venky4378 <venkyswamy437@gmail.com>
2026-03-26 13:14:43 +0530

    2nd commit

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+class Hero{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 15;
+		int sum = a + b;
+		int subtract = a-b;
+		int multiple = a * b ;
+		int division = a/b;
+		
+		System.out.println(sum);
+		
+	}
+}
---
+++ firstclass.java
@@ -0,0 +1,15 @@
+class Hero{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 15;
+		int sum = a + b;
+		int subtract = a-b;
+		int multiple = a * b ;
+		int division = a/b;
+		
+		System.out.println(sum);
+		System.out.println(subtract);
+		System.out.println(multiple);
+		System.out.println(division);
+	}
+}
